f37e254c5207b0a52feb5508dfe0083d25c123bd
Chính <87134996+vchinh1030@users.noreply.github.com>
2023-02-15 21:38:35 +0700

    up to develop

---
+++ Web_CSE/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Web_CSE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
