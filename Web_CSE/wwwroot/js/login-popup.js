a3219b1e202b41762faa107ee1bd0c9f815b8248
Chính <chinhtogaming05@gmail.com>
2023-03-05 23:39:07 +0700

    fix

---
+++ Web_CSE/wwwroot/js/login-popup.js
@@ -0,0 +1,29 @@
+const loginBtn = document.querySelectorAll('.btn-login');
+const modalClose = document.querySelector('.modal-close');
+const modal = document.querySelector('.modal');
+const modalContainer = document.querySelector('.modal-container');
+const fullSizeModal = document.querySelector('.modal-full');
+function showModal() {
+    modal.classList.add('open');
+}
+function hideModal() {
+    modal.classList.remove('open');
+    modalContainer.classList.remove('full-screen');
+}
+function FullSize() {
+
+    modalContainer.classList.toggle('full-screen');
+}
+
+for (const btn of loginBtn) {
+    btn.addEventListener('click', showModal);
+};
+
+modalClose.addEventListener('click', hideModal);
+
+modal.addEventListener('click', hideModal)
+modalContainer.addEventListener('click', function (e) {
+    e.stopPropagation();
+})
+
+fullSizeModal.addEventListener('click', FullSize);
