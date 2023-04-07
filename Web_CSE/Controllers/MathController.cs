a52afa3b9f9b7f2a9e3f1923669569e5d0c829c3
Nguyen Cong Thang <1951061015@e.tlu.edu.vn>
2023-04-07 12:00:55 +0700

    up

---
+++ Web_CSE/Controllers/MathController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Web_CSE.Models;
+
+namespace Web_CSE.Controllers
+{
+    public class MathController : Controller
+    {
+        private readonly CnttCseContext _context;
+
+        public MathController(CnttCseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Math
+        public async Task<IActionResult> Index()
+        {
+            var cnttCseContext = _context.Posts.Where(p => p.CatId == 28);
+            return View(await cnttCseContext.ToListAsync());
+        }
+    }
+}
---
+++ Web_CSE/Controllers/SRprojectsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Web_CSE.Models;
+
+namespace Web_CSE.Controllers
+{
+    public class SRprojectsController : Controller
+    {
+        private readonly CnttCseContext _context;
+
+        public SRprojectsController(CnttCseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: SRprojects
+        public async Task<IActionResult> Index()
+        {
+            var cnttCseContext = _context.Posts.Where(p => p.CatId == 16);
+            return View(await cnttCseContext.ToListAsync());
+        }
+
+        
+    }
+}
